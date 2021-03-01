2ce8fc5d00aab9ade1309f646e8930e389dfab8f
vcrl <vincentruelle.pro@gmail.com>
2021-03-01 14:39:39 +0100

    added templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 def signup(request):
-    return render(request, "accounts/signup.html", {'form':UserCreationForm()})
+    if request.method == "GET":
+        return render(request, "accounts/signup.html", {'form':UserCreationForm()})
+    else:
+        if request.POST["password1"] == request.POST["password2"]:
+            newuser = User.objects.create_user(request.POST["username"], None, request.POST["password1"])
+            User.save(newuser)
+        else:
+            print("not matching")
