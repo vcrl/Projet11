de21c09e581c14e3e5dfcc772173395e78fa8081
vcrl <vincentruelle.pro@gmail.com>
2021-03-08 09:42:27 +0100

    refactorized

--- research/management/commands/insert_products.py
+++ research/management/commands/insert_products.py
@@ -24,7 +24,38 @@ class Command(BaseCommand):
         for category in categories:
             products = self.get_products(category.name)
             self.insert_products_in_db(products, category)
-        
+
+    def get_products(self, category):
+        payload = {
+                'action': 'process',
+                'tagtype_0': 'categories',
+                'tag_contains_0': 'contains',
+                'tag_0': category,
+                'tagtype_1': 'nutrition_grade',
+                'tag_contains_1': 'contains',
+                'page_size': 100,
+                'json': '1',
+                }
+
+        response = requests.get(self.api_url, params=payload)
+        response_as_json = response.json()
+        product_to_add = []
+        for product in response_as_json['products']:
+            try:
+                product_values = {
+                    "name": product['product_name'],
+                    "brand": product['brands'],
+                    "category": category,
+                    "url": product['url'],
+                    "img_url": product['image_url'],
+                    "nutriscore": product['nutrition_grades'],
+                }
+                product_to_add.append(product_values)
+            except KeyError:
+                pass
+            
+        return product_to_add
+
     def insert_products_in_db(self, products, category):
         for product in products:
             try:
@@ -51,36 +82,3 @@ class Command(BaseCommand):
                 name = category
             )
             add.save()
-
-    def get_products(self, category):
-
-        payload = {
-                'action': 'process',
-                'tagtype_0': 'categories',
-                'tag_contains_0': 'contains',
-                'tag_0': category,
-                'tagtype_1': 'nutrition_grade',
-                'tag_contains_1': 'contains',
-                'page_size': 100,
-                'json': '1',
-                }
-
-        response = requests.get(self.api_url, params=payload)
-        response_as_json = response.json()
-        product_to_add = []
-        for product in response_as_json['products']:
-            
-            try:
-                product_values = {
-                    "name": product['product_name'],
-                    "brand": product['brands'],
-                    "category": category,
-                    "url": product['url'],
-                    "img_url": product['image_url'],
-                    "nutriscore": product['nutrition_grades'],
-                }
-                product_to_add.append(product_values)
-            except KeyError:
-                pass
-            
-        return product_to_add
