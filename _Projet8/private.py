af8f16d6249003ec8f5f5d672258d87675983c59
vcrl <vincentruelle.pro@gmail.com>
2021-03-23 20:01:04 +0100

    setting email login

--- _Projet8/private.py
+++ _Projet8/private.py
@@ -4,7 +4,7 @@ KEY = '84-d)vu-dnzpeddj2f18#_e!v&5=v$$3012(shh7m9aad0iac)'#os.environ.get("DJANG
 DB = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',#os.environ.get("DB_ENGINE"),
-        'NAME': 'projet8',#os.environ.get("DB_NAME"),
+        'NAME': 'projet_8',#os.environ.get("DB_NAME"),
         'USER': 'postgres',#os.environ.get("DB_USER"),
         'PASSWORD' : '123',#os.environ.get("DB_PASSWORD"),
         'HOST': 'localhost',#os.environ.get("DB_HOST"),
