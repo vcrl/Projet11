5c393c62e223b18a62dcb3ccec8bf05552114051
vcrl <vincentruelle.pro@gmail.com>
2021-03-23 18:58:45 +0100

    added login with email

--- _Projet8/private.py
+++ _Projet8/private.py
@@ -3,11 +3,11 @@ import os
 KEY = os.environ.get("DJANGO_KEY")
 DB = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'projet_8',
-        'USER': 'postgres',
-        'PASSWORD' : '123',
-        'HOST': 'localhost',
-        'PORT': '5432' 
+        'ENGINE': os.environ.get("DB_ENGINE"),
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD' : os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT") 
     }
 }
