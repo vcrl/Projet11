b892f325799c32db8d788627a8ae23ec2fa32987
vcrl <vincentruelle.pro@gmail.com>
2021-03-23 12:54:39 +0100

    add prod settings

--- _Projet8/private.py
+++ _Projet8/private.py
@@ -1,6 +1,6 @@
 import os
 
-KEY = os.environ.get("DJANGO_KEY")#'84-d)vu-dnzpeddj2f18#_e!v&5=v$$3012(shh7m9aad0iac)'
+KEY = os.environ.get("DJANGO_KEY")
 DB = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
