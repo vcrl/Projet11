a2ae006e43bda5df5d360b22d46b1228ca975702
vcrl <vincentruelle.pro@gmail.com>
2021-03-25 13:30:36 +0100

    tests ok

--- _Projet8/private.py
+++ _Projet8/private.py
@@ -1,13 +1,13 @@
 import os
 
-KEY = '84-d)vu-dnzpeddj2f18#_e!v&5=v$$3012(shh7m9aad0iac)'#os.environ.get("DJANGO_KEY")
+KEY = os.environ.get("DJANGO_KEY")
 DB = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',#os.environ.get("DB_ENGINE"),
-        'NAME': 'projet_8',#os.environ.get("DB_NAME"),
-        'USER': 'postgres',#os.environ.get("DB_USER"),
-        'PASSWORD' : '123',#os.environ.get("DB_PASSWORD"),
-        'HOST': 'localhost',#os.environ.get("DB_HOST"),
-        'PORT': '5432'#os.environ.get("DB_PORT") 
+        'ENGINE': os.environ.get("DB_ENGINE"),
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD' : os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT") 
     }
 }
